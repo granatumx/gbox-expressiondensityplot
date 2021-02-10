bf1289876948e1c87fe6d6c9c2ba9ee7a06a73a0
GranatumX <granatumx@yahoo.com>
2021-02-10 13:58:47 -0500

    Update

--- expressiondensityplot.py
+++ expressiondensityplot.py
@@ -12,10 +12,8 @@ from itertools import cycle
 import seaborn as sns
 from scipy.stats import poisson
 from scipy.optimize import least_squares
-import seaborn as sns
 import statistics as s
 import statsmodels.stats.api as sms
-
 
 from sklearn.mixture import GaussianMixture as GM
 
@@ -25,6 +23,7 @@ import sys
 
 from gbox_py_helpers import bug_report
 
+
 def confint(X, alpha=0.05):
     resultdict = {}
     meanbounds = sms.DescrStatsW(X).tconfint_mean(alpha=alpha)
@@ -33,6 +32,7 @@ def confint(X, alpha=0.05):
     resultdict["n"] = len(X)
     return resultdict
 
+
 def dist(int1, int2):
     if int1["low"] >= int2["high"]:
         return int1["low"] - int2["high"]
@@ -40,8 +40,8 @@ def dist(int1, int2):
         return int2["low"] - int1["high"]
     return 0.0
 
-# return hash of labels associated to its data
-def trygmonvector(gm, X):
+
+def trygmonvector(gm, X):   # return hash of labels associated to its data
     vectors = gm.predict(np.array(X).reshape(-1, 1))
     inv_map = {}
     for i, v in enumerate(vectors):
@@ -51,19 +51,41 @@ def trygmonvector(gm, X):
 
 def model1(t, coeffs):
     lamb1 = coeffs[0]
-    return poisson.pmf(np.round(t), lamb1)
+    eval1 = poisson.pmf(np.floor(t), lamb1)
+    eval2 = poisson.pmf(np.ceil(t), lamb1)
+    return eval1 + (t - np.floor(t)) * (eval2 - eval1)
+
 
 def residuals1(coeffs, y, t):
     return y - model1(t, coeffs)
+
 
 def model2(t, coeffs):
     a = coeffs[0]
     lamb1 = coeffs[1]
     lamb2 = coeffs[2]
-    return (a*poisson.pmf(np.round(t), lamb1)+poisson.pmf(np.round(t), lamb2))/(1.0+a)
+    return (a * model1(t, [lamb1]) + model1(t, [lamb2])) / (1.0 + a)
+
 
 def residuals2(coeffs, y, t):
     return y - model2(t, coeffs)
+
+
+def model3_fix_params(lamb1, lamb2):
+    def model3(t, coeffs):
+        a = coeffs[0]
+        return model2(t, [a, lamb1, lamb2])
+
+    return model3
+
+
+def residuals3_fix_params(lamb1, lamb2):
+    model3 = model3_fix_params(lamb1, lamb2)
+
+    def residuals3(coeffs, y, t):
+        return y - model3(t, coeffs)
+
+    return residuals3
 
 
 def fit_data_two_poissons(X, initial):
@@ -80,6 +102,15 @@ def fit_data_one_poisson(X, initial):
     return parameters
 
 
+def fit_poissons_fixed_means(X, lamb1, lamb2):
+    residuals3 = residuals3_fix_params(lamb1, lamb2)
+    entries, bins = np.histogram(X, bins="auto", density=True)
+    bins = (bins[1:] + bins[:-1]) / 2
+    parameters = least_squares(residuals3, [0.5], args=(entries, bins))
+    parameters.x = np.append(parameters.x, [lamb1, lamb2])
+    return parameters
+
+
 def one_or_two_mixtures(X, alpha=0.05, min_dist=0.2, min_zscore=2):
     column = np.array(X).reshape(-1, 1)
     gm = GM(n_components=2).fit(column)
@@ -90,59 +121,106 @@ def one_or_two_mixtures(X, alpha=0.05, min_dist=0.2, min_zscore=2):
     if len(inv_map) <= 1 or len(inv_map[0]) < 3 or len(inv_map[1]) < 3:
         gm = GM(n_components=1).fit(column)
         mi = confint(X)
-        return {"data": X, "mean": mean, "std": std, "gm": gm, "low_means": [mi["low"]], "high_means": [mi["high"]], "n": [len(X)]}
+        return {"data": X, "mean": mean, "std": std, "gm": gm, "low_means": [mi["low"]], "high_means": [mi["high"]],
+                "n": [len(X)]}
 
     mi1 = confint(inv_map[0], alpha=alpha)
     mi2 = confint(inv_map[1], alpha=alpha)
     # zscore1 = abs(s.mean(inv_map[0])-s.mean(inv_map[1]))/(s.stdev(inv_map[1])+1e-16)
     # zscore2 = abs(s.mean(inv_map[1])-s.mean(inv_map[0]))/(s.stdev(inv_map[0])+1e-16)
-    if dist(mi1, mi2) <= min_dist or abs(gm.means_[1][0]-gm.means_[0][0])/(max(gm.covariances_)[0][0]) < min_zscore:
+    if dist(mi1, mi2) <= min_dist or abs(gm.means_[1][0] - gm.means_[0][0]) / (max(gm.covariances_)[0][0]) < min_zscore:
         gm = GM(n_components=1).fit(column)
         mi = confint(X)
-        result = {"data": X, "mean": mean, "std": std, "gm": gm, "low_means": [mi["low"]], "high_means": [mi["high"]], "n": [len(X)]}
+        result = {"data": X, "mean": mean, "std": std, "gm": gm, "low_means": [mi["low"]], "high_means": [mi["high"]],
+                  "n": [len(X)]}
     elif mi1["low"] < mi2["low"]:
-        result = {"data": X, "mean": mean, "std": std, "gm": gm, "label_order": [0, 1], "low_means": [mi1["low"], mi2["low"]], "high_means": [mi1["high"], mi2["high"]], "n": [mi1["n"], mi2["n"]]}
+        result = {"data": X, "mean": mean, "std": std, "gm": gm, "label_order": [0, 1],
+                  "low_means": [mi1["low"], mi2["low"]], "high_means": [mi1["high"], mi2["high"]],
+                  "n": [mi1["n"], mi2["n"]]}
     else:
-        result = {"data": X, "mean": mean, "std": std, "gm": gm, "label_order": [1, 0], "low_means": [mi2["low"], mi1["low"]], "high_means": [mi2["high"], mi1["high"]], "n": [mi2["n"], mi1["n"]]}
+        result = {"data": X, "mean": mean, "std": std, "gm": gm, "label_order": [1, 0],
+                  "low_means": [mi2["low"], mi1["low"]], "high_means": [mi2["high"], mi1["high"]],
+                  "n": [mi2["n"], mi1["n"]]}
     return result
 
-# First transform X into log(X)+c such that it does not go below 0
-# X is a list
+
+# First transform X into log(X)+c such that it does not go below 0, X is a list
 def fit_poissons(X, alpha=0.05, min_dist=0.2, min_zscore=2):
-    shift = np.min(X) - 1                  # Needed later to shift back
+    shift = np.min(X) - 1  # Needed later to shift back
     Xarr = np.log(X - shift)
     res = one_or_two_mixtures(Xarr.tolist(), alpha=0.05, min_dist=min_dist, min_zscore=min_zscore)
     numcomponents = len(res["low_means"])
     if numcomponents == 2:
-        mean1 = 0.5*(res["low_means"][0] + res["high_means"][0])
-        mean2 = 0.5*(res["low_means"][1] + res["high_means"][1])
+        mean1 = 0.5 * (res["low_means"][0] + res["high_means"][0])
+        mean2 = 0.5 * (res["low_means"][1] + res["high_means"][1])
         mean1 = np.exp(mean1) + shift
         mean2 = np.exp(mean2) + shift
         sz1 = res["n"][0]
         sz2 = res["n"][1]
-        alpha = sz1/(sz1+sz2)
+        alpha = sz1 / sz2
         # Now optimize with estimates
-        coeffs = fit_data_two_poissons(X, [alpha, mean1, mean2])
-        if coeffs.x[0] > 0.0 and coeffs.x[0] < 1.0:
-            return {"n":2, "coeffs":coeffs}
-    mean1 = np.exp(0.5*(res["low_means"][0] + res["high_means"][0])) + shift
-    coeffs = fit_data_one_poisson(X, [mean1])
-    return {"n":1, "coeffs":coeffs}
+        # coeffs = fit_data_two_poissons(X, [alpha, mean1, mean2])
+        coeffs_fm = fit_poissons_fixed_means(X, mean1, mean2)
+        print("Optimality fm = {}".format(coeffs_fm.cost))
+        coeffs_2 = fit_data_two_poissons(X, [alpha, mean1, mean2])
+        print("Optimality 2 = {}".format(coeffs_2.cost))
+        coeffs_1 = fit_data_one_poisson(X, [np.mean(X)])
+        print("Optimality 1 = {}".format(coeffs_1.cost))
+        if coeffs_2.cost < coeffs_fm.cost:
+            coeffs = coeffs_2
+        else:
+            coeffs = coeffs_fm
+        if coeffs.x[0] > 0.0 and 2 * coeffs.cost < coeffs_1.cost:
+            return {"n": 2, "coeffs": coeffs}
 
-def plot_fits(row, alpha=0.05, min_dist=0.2, min_zscore = 2):
-    params = fit_poissons(row, alpha=alpha, min_dist=min_dist, min_zscore=min_zscore)
-    X, bins = np.histogram(row, bins=int(np.round(np.max(row)-np.min(row))), density=True)
+    print("Only have one!")
+    mean1 = np.mean(X)
+    mean2 = mean1 + min_zscore * np.sqrt(mean1)
+    mean1 = np.mean(X[X < mean2 - np.sqrt(mean2) / 2.0])
+
+    coeffs = fit_poissons_fixed_means(X, mean1, mean2)
+    print("Alpha = {}".format(coeffs.x[0]))
+    # coeffs = fit_data_one_poisson(X, [mean1])
+    return {"n": 2, "coeffs": coeffs}
+
+
+def percent_upregulation(a):
+    return 100.0*(1.0/(a+1.0))
+
+
+def get_weight_with_statistic(row, params):   # Returns a
+    coeffs_fm = fit_poissons_fixed_means(row, params["coeffs"].x[1], params["coeffs"].x[2])   # Use the statistic means
+    print("Optimality fm = {}".format(coeffs_fm.cost))
+    return coeffs_fm.x[0]
+
+
+def plot_two_poissons_from_params(row, params, color="r", label=""):   # Return the bins
+    X, bins = np.histogram(row, bins="auto", density=True)   # Sample histogram to pull out bins
     bins = (bins[1:] + bins[:-1]) / 2.0
-    sns.kdeplot(row, shade=True)
-    if params["n"] == 1:
-        plt.plot(bins, model1(bins, params["coeffs"].x), "rx")
-    else:
-        plt.plot(bins, model2(bins, params["coeffs"].x), "gx")
-        a = params["coeffs"].x[0]
-        sc = (a+1.0)/a
-        mn1 = params["coeffs"].x[1]
-        plt.plot(bins, model1(bins, [mn1])/sc, "bx")
-        print("Standard deviation = {}".format(np.sqrt(mn1)))
+    sns.kdeplot(row, shade=False, label=label)                 # Plot histogram for cluster
+    plt.axvline(x=params["coeffs"].x[1], color=color)          # Show where the ref's distributions are
+    plt.axvline(x=params["coeffs"].x[2], color=color)          # Show where the ref's distributions are
+    return bins
+
+
+def plot_poisson(row, params, color="r", alpha=0.05, min_dist=0.2, min_zscore = 2, label=""):
+    a = get_weight_with_statistic(row, params)
+    params["coeffs"].x[0] = a                   # Update params with fitted a, this may side-effect
+    pt_up = percent_upregulation(a)
+    plot_two_poissons_from_params(row, params, label=label+" +{:.0f}%".format(pt_up))
+    return pt_up
+
+
+def plot_fits(row, color="r", alpha=0.05, min_dist=0.2, min_zscore = 2, label=""):  # Return the fitted params
+    params = fit_poissons(row, alpha=alpha, min_dist=min_dist, min_zscore=min_zscore)
+    pt_up = percent_upregulation(params["coeffs"].x[0])
+    bins = plot_two_poissons_from_params(row, params, label=label+" +{:.0f}%".format(pt_up))
+    # plt.plot(bins, model2(bins, params["coeffs"].x), color+"x", label="Poisson fit")
+    # a = params["coeffs"].x[0]
+    # sc = (a+1.0)/a
+    # mn2 = params["coeffs"].x[2]
+    # plt.plot(bins, model1(bins, [mn2])/sc, "bx", label="Up-regulated fit")      # Show upregulated total
+    return params
 
 
 def parse(st):
@@ -165,20 +243,22 @@ def main():
         inv_map[v] = inv_map.get(v, []) + [k]
 
     for gene in gene_ids:
+        plt.figure()
+        # First form a statistic for all values, also puts out plot
+        params = plot_fits(df.loc[gene, :], color="r", alpha=alpha, min_dist=min_dist, min_zscore=min_zscore, label="All")
         for k, v in inv_map.items():
-            plt.figure()
 
             plt.subplot(1, 1, 1)
-            plt.title('Gene expression level distribution')
-            plot_fits(df.loc[gene,v].to_list(), alpha=0.05, min_dist=0.1, min_zscore=2)
+            plt.title('Gene expression level distribution for each cluster')
+            plot_poisson(df.loc[gene, v].to_list(), params, label=k)
             # sns.distplot(df.loc[gene,:].to_list(), bins=int(100), color = 'darkblue', kde_kws={'linewidth': 2})
             plt.ylabel('Frequency')
             plt.xlabel('Gene expression')
 
-            plt.tight_layout()
+        plt.tight_layout()
 
-            caption = ( "The distribution of expression levels for gene {} and cluster {}.".format(gene, k))
-            gn.add_current_figure_to_results(caption, zoom=1, dpi=75)
+        caption = ( "The distribution of expression levels for gene {}.".format(gene))
+        gn.add_current_figure_to_results(caption, zoom=1, dpi=75)
 
     gn.commit()
 
