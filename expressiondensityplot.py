bbbeb2e69a747840e2a585b296615fcc254a181e
GranatumX <granatumx@yahoo.com>
2021-02-10 14:20:22 -0500

    Update

--- expressiondensityplot.py
+++ expressiondensityplot.py
@@ -196,12 +196,13 @@ def get_weight_with_statistic(row, params):   # Returns a
 
 
 def plot_two_poissons_from_params(row, params, color="r", label=""):   # Return the bins
-    X, bins = np.histogram(row, bins="auto", density=True)   # Sample histogram to pull out bins
-    bins = (bins[1:] + bins[:-1]) / 2.0
-    sns.kdeplot(row, shade=False, label=label, kde_kws={'bw':1})                 # Plot histogram for cluster
+    bins = np.arange(np.min(row), np.max(row))
+    X, bins = np.histogram(row, bins=bins, density=True)   # Sample histogram to pull out bins
+    mbins = (bins[1:] + bins[:-1]) / 2.0
+    sns.kdeplot(row, bins=bins, shade=False, label=label, kde_kws={'bw':1})                 # Plot histogram for cluster
     plt.axvline(x=params["coeffs"].x[1], color=color)          # Show where the ref's distributions are
     plt.axvline(x=params["coeffs"].x[2], color=color)          # Show where the ref's distributions are
-    return bins
+    return mbins
 
 
 def plot_poisson(row, params, color="r", alpha=0.05, min_dist=0.2, min_zscore = 2, label=""):
