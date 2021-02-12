946ea16f51c127db0bfe67b59c0feff316b0cfcd
GranatumX <granatumx@yahoo.com>
2021-02-11 20:21:53 -0500

    Update

--- expressiondensityplot.py
+++ expressiondensityplot.py
@@ -201,7 +201,7 @@ def get_poisson_weight_with_statistic(row, params):   # Returns a
 
 
 def plot_hist(row, label=""):
-    sns.histplot(row, binwidth=1.0, label=label, element="poly", fill=False, kde=True)                 # Plot histogram for cluster
+    sns.histplot(row, binwidth=1.0, label=label, element="step", fill=False)                 # Plot histogram for cluster
 
 
 def plot_two_poissons_from_params(row, params, color="r", label=""):   # Return the bins
